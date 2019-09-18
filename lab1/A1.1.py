0db6dad6b5f647dce35192da51278e307be73ef8
Michael Vu <michael.vu.97@outlook.com>
2019-09-18 17:43:43 -0400

    added part d

--- lab1/A1.1.py
+++ lab1/A1.1.py
@@ -1,30 +1,50 @@
 import random
 import matplotlib.pyplot as plot
 
-N = 100
-
 true_expected_mean = 1.25
 true_expected_variance = 37.0/48 
 
-def probably():
-    p = random.random() # in [0,1)
-    if p <= 0.5:
-        return random.uniform(0, 1)
-    else:
-        return random.uniform(1,3)
+def run(N, showPlot):
+    def probably():
+        p = random.random() # in [0,1)
+        if p <= 0.5:
+            return random.uniform(0, 1)
+        else:
+            return random.uniform(1,3)
 
-results = []
+    results = []
 
-for i in range(N):
-    results.append(probably())
+    for i in range(N):
+        results.append(probably())
 
-plot.hist(results)
+    if showPlot:
+        plot.hist(results)
+        plot.show()
 
-plot.show()
+    av = sum(results) / N
 
-av = sum(results) / N
+    variance = sum(map(lambda x: ((x - av)**2), results)) / N;
+    return av, variance;
 
-variance = sum(map(lambda x: ((x - av)**2), results)) / N;
+
+# Part a-c
+av, variance = run(100, True)
 
 print("average: {} ({} from true), variance: {} ({} from true)".format(av, av - true_expected_mean, variance, variance - true_expected_variance))
 
+# part d
+Ns = [100, 200, 300, 400, 500, 1000, 2000, 5000]
+
+averages = []
+variances = []
+
+for n in Ns:
+    curr_av, curr_variance = run(n, False)
+    averages.append(curr_av)
+    variances.append(curr_variance)
+
+plot.plot(Ns, averages)
+plot.show()
+
+plot.plot(Ns, variances)
+plot.show()
