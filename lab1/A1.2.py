f77c5e0639bc3edb0d8d9f5ac2e7c4ab41588f6c
Michael Vu <michael.vu.97@outlook.com>
2019-09-19 14:53:19 -0400

    finished 2.a,b

--- lab1/A1.2.py
+++ lab1/A1.2.py
@@ -1,2 +1,32 @@
 import random
 import matplotlib.pyplot as plot
+
+def probably():
+    r = random.random()
+
+    if r <= 0.25:
+        return 1
+    if r <= 0.5:
+        return 2
+    if r <= 0.5 + 0.125:
+        return 3
+    if r <= 0.5 + (0.125 * 2):
+        return 4
+    if r <= 0.5 + (0.125 * 3):
+        return 5
+
+    return 6
+
+N = 1000000
+
+pmf_sampled = [[0 for i in range(6)] for j in range(6)]
+
+for i in range(N):
+    X = probably()
+    Y = probably()
+    pmf_sampled[X - 1][Y - 1] += 1
+
+print pmf_sampled
+
+plot.imshow(pmf_sampled)
+plot.show()
