68b325f49508e71a6bef61dcbe195ba70b06b313
Michael Vu <michael.vu.97@outlook.com>
2019-10-02 11:19:11 -0400

    finished 2.b,c

--- lab2/2.b.py
+++ lab2/2.b.py
@@ -1,6 +1,7 @@
 import random
 import math
 import matplotlib.pyplot as plot
+import numpy as np
 
 samples = []
 
@@ -13,10 +14,26 @@ def sample(n):
     return total
 
 def Z():
+    # Will adjust the sample to become zero-mean and unit variance.
     return (sample(100) - 50) / math.sqrt(100/12)
 
 for n in range(N):
     samples.append(Z())
 
-plot.hist(samples)
+def N_pdf(x):
+    variance = 1
+    mean = 0
+
+    return (1/ math.sqrt(2 * math.pi * variance)) * math.exp((-((x - mean) **2))  / (2 * variance));
+
+# generate a gaussian pdf
+gaussian_x = np.linspace(-4, 4, 1000)
+gaussian_y = [N_pdf(x) for x in gaussian_x]
+
+fig,ax1 = plot.subplots()
+
+ax1.hist(samples, bins=20)
+
+ax2 = ax1.twinx()
+ax2.plot(gaussian_x, gaussian_y, color='red')
 plot.show()
