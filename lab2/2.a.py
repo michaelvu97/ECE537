876100e1471703830698b006721fdbdb6105a630
Michael Vu <michael.vu.97@outlook.com>
2019-10-02 00:40:36 -0400

    finished 2.a

---
+++ lab2/2.a.py
@@ -0,0 +1,19 @@
+import random
+import math
+import matplotlib.pyplot as plot
+
+vals = [0]
+
+N = 1000
+
+def sample(n):
+    total = 0
+    for i in range(n):
+        total += random.random()
+    return total
+
+for n in range(1, N + 1):
+    vals.append(sample(n) / n)
+
+plot.plot(vals)
+plot.show()
