4cef4890ccbcfb0251906ee07cb2d2c7812aaa0f
Michael Vu <michael.vu.97@outlook.com>
2019-10-30 22:32:11 -0400

    fixes to sampling in 2

--- lab3/2.py
+++ lab3/2.py
@@ -1,10 +1,26 @@
 import numpy as np
 import random
+import math
 import matplotlib.pyplot as plot
 
 def SampleExp(rate):
     u = random.random()
-    return - np.log(u / rate) / rate
+    return - np.log(u) / rate
+
+def PoissonPmf(rate, t, limit):
+    result = []
+    for k in range(limit):
+        result.append(np.exp(-rate * t) * ((rate  * t) ** k) / math.factorial(k))
+
+    return result
+
+"""
+FX(x)=1-exp(-rate * x)
+x=1-exp(-rate * y)
+(1-x)=exp(-rate * y)
+ln(1-x)=-rate * y
+FX(x) = -ln(1-u)/rate
+"""
 
 def N(t, rate, sample_limit):
     count = 0
@@ -19,14 +35,32 @@ def N(t, rate, sample_limit):
 
     return count, X_sample_hist
 
-samples_a = [N(5, 2, 20)[1] for n in range(5)]
+rate = 2
+t = 5
+limit = 20
+
+samples_a = [N(t, rate, limit)[1] for n in range(5)]
 
 for sample in samples_a:
     plot.plot(sample)
 
 plot.show()
 
-samples_b = [N(5, 2, 20)[0] for n in range(1000)]
 
-plot.hist(samples_b)
+
+samples_b = [N(t, rate, limit)[0] for n in range(1000)]
+
+poisson_pmf = PoissonPmf(rate, t, 20)
+print(poisson_pmf)
+
+fig,ax1 = plot.subplots()
+ax1.hist(samples_b)
+
+ax2 = ax1.twinx()
+ax2.plot(poisson_pmf, color='red')
+
+# ax1.set_xlabel('Z_100')
+# ax1.set_ylabel('Count')
+# ax2.set_ylabel('Unit Gaussian PDF')
 plot.show()
+
